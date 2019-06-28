11aecf48bab6a6562a60a5ddf156711cf3f23f67
sakurai <kd1282334@st.kobedenshi.ac.jp>
2019-06-28 09:53:30 +0900

    6/28 課題

--- 01.js
+++ 01.js
@@ -1,10 +1,22 @@
 // FizzBuzz
 
 // サンプル: "1" という文字列を出力
-console.log("1");
+//console.log("1");
 
 // サンプル: "FizzBuzz" という文字列を出力
 // (process.stdout.write で改行なしの出力ができる)
-process.stdout.write("Fizz");
-process.stdout.write("Buzz");
-console.log();
+//process.stdout.write("Fizz");
+//process.stdout.write("Buzz");
+//console.log();
+
+for(var i = 1; i <= 15; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz");
+    }else if(i % 3 == 0){
+        console.log("Fizz");
+    }else if(i % 5 == 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
