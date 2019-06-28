f39fe0662602187fc6ace83ec2c0f4f730cf7ce0
sakurai <kd1282334@st.kobedenshi.ac.jp>
2019-06-28 10:40:16 +0900

    6/28 発展課題

---
+++ 02.js
@@ -0,0 +1,23 @@
+for (var i = 1896; i <= 2020; i += 4){
+    switch(i){
+        case 1916:
+            break;
+        
+        case 1940:
+            break;
+
+        case 1944:
+            break;
+
+        case 2020:
+            console.log("第32回　東京でもうすぐ！");
+            break;
+
+        default:
+            var times = (i - 1892) / 4;
+            process.stdout.write("第" + times + "回");
+            console.log(i + "年");
+            break;
+
+    }
+}
